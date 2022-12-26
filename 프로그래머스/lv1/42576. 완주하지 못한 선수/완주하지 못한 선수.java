f65afbbebbdfcee76aa1c3d8017c837aba88bd23
LeeGoh <107545016+LeeGoh@users.noreply.github.com>
2022-12-26 13:34:58 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 289.17 ms, Memory: 95.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        int count = 0;
+        
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        
+        answer = participant[participant.length-1];
+        
+        for(int i = 0; i < completion.length; i++){
+            if(!participant[i].equals(completion[i])) {
+                answer = participant[i];
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
