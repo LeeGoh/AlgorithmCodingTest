661e181336649924a07af1bf717e362fc80de9de
LeeGoh <107545016+LeeGoh@users.noreply.github.com>
2023-01-16 11:03:16 +0900

    [unrated] Title: 기사단원의 무기, Time: 0.47 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+        int count = 0;
+
+        for(int i = 1; i <= number; i++){
+            count = 0;
+            for(int j = 1; j*j <= i; j++){
+                if(j * j == i) count++;
+                else if(i % j == 0) count += 2;
+            }
+            if(count > limit) count = power;
+            answer += count;
+        }
+        return answer;
+    }
+}
